eab9617ae02db35bbc530eafb157703f61b05cc8
tawsif1 <60055830+tawsif1@users.noreply.github.com>
2020-10-31 15:06:30 -0600

    Updates

---
+++ precache-manifest.6ead8ba7214e79aa6d181ca7b37fdebe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "276f0ac8f5635a17f025e3317f4fb067",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6d7c4ce73daa3424b01a",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "8332c687e218aa95c1f2",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6d7c4ce73daa3424b01a",
+    "url": "/portfolio/static/js/2.1b6af01b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.1b6af01b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8332c687e218aa95c1f2",
+    "url": "/portfolio/static/js/main.ed96e56d.chunk.js"
+  },
+  {
+    "revision": "b37b5bb5a98282a2f0e7",
+    "url": "/portfolio/static/js/runtime-main.b449097c.js"
+  },
+  {
+    "revision": "985b4eecacba9dd02d825ff16129e9e4",
+    "url": "/portfolio/static/media/Tawsif Ahmed Logo.985b4eec.png"
+  },
+  {
+    "revision": "1e01f0c0dea217ba568ebbcec49c5b6b",
+    "url": "/portfolio/static/media/linkedin.1e01f0c0.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2ba8d92f2126d0ae1763ec73af51efe4",
+    "url": "/portfolio/static/media/others.2ba8d92f.png"
+  },
+  {
+    "revision": "66ce42a201432d54cbeea2eaffe666f4",
+    "url": "/portfolio/static/media/portfolio.66ce42a2.png"
+  },
+  {
+    "revision": "442c6c5d41cd71063d5934223aa05ef9",
+    "url": "/portfolio/static/media/resume.442c6c5d.png"
+  }
+]);
